f5ac0135a9a53542dc1c4a4a9a00a3cf2c319b67
atn95 <antannguyen2000@gmail.com>
2022-08-31 21:01:29 -0500

    project readme

--- src/main/java/main/Server/controller/ServerController.java
+++ src/main/java/main/Server/controller/ServerController.java
@@ -2,16 +2,25 @@ package main.Server.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping(value = "/api")
 public class ServerController {
 
     @Autowired
     public ServerController() {
     }
-    @GetMapping
+    @GetMapping(value = "/hello")
     public String test() {
         return "Server is running";
+    }
+
+    @GetMapping(value = "/list")
+    public int[] getList() {
+        return new int[]{1,2,3,4,6,7};
     }
 }
