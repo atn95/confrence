a19fbc2222805e6c6c35003cb97765fa0eccf799
atn95 <antannguyen2000@gmail.com>
2022-09-04 00:12:10 -0500

    sending to specific room

--- src/main/java/main/server/socket/messages/MessageController.java
+++ src/main/java/main/server/socket/messages/MessageController.java
@@ -1,17 +1,27 @@
 package main.server.socket.messages;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class MessageController {
+    private SimpMessagingTemplate template;
 
-    @MessageMapping("/message")//client sent to 'ws/messages'
-    @SendTo("/topic/messages")//client listening to this topic
-    public ResponseMessage getMessage(final ChatMessage message) {
+    @Autowired
+    public MessageController(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
+    @MessageMapping("/message/{room}")//client sent to 'ws/messages/{room}'
+    //@SendTo("/topic/messages")//client listening to this topic
+    public void getMessage(@DestinationVariable int room, final ChatMessage message) {
         System.out.println("Message \n \n " + message.getMessageContent() +" \n \n *******");
-        return new ResponseMessage(HtmlUtils.htmlEscape(message.getMessageContent()));
+        ResponseMessage resp = new ResponseMessage(HtmlUtils.htmlEscape(message.getMessageContent()));
+        this.template.convertAndSend("/topic/room/" + room, resp);
     }
 }
