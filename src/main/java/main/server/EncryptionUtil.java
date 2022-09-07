c8dc6bd07157ca56f617807185d7a4ae5e181b19
atn95 <antannguyen2000@gmail.com>
2022-09-06 21:34:38 -0500

    JWT token send and login works, returns bearer token

--- src/main/java/main/server/EncryptionUtil.java
+++ src/main/java/main/server/EncryptionUtil.java
@@ -3,6 +3,7 @@ package main.server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
--- src/main/java/main/server/account/AccountController.java
+++ src/main/java/main/server/account/AccountController.java
@@ -1,19 +1,34 @@
 package main.server.account;
 
+import main.server.EncryptionUtil;
+import main.server.JwtTokenUtil;
 import main.server.constants.ServerConstants;
 import main.server.utils.exception.ApiException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = ServerConstants.API_ROUTE + "/user")
 public class AccountController {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private final JwtTokenUtil jwtTokenUtil;
+
     public final AccountService accountService;
 
     @Autowired
-    public AccountController(AccountService accountService) {
+    public AccountController(AccountService accountService, AuthenticationManager authenticationManager, JwtTokenUtil jwtTokenUtil) {
         this.accountService = accountService;
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenUtil = jwtTokenUtil;
     }
     @GetMapping
     public String getRoute() {
@@ -34,15 +49,18 @@ public class AccountController {
         try {
            return accountService.registerAccount(newAccount);
         } catch (Exception e) {
-            throw new ApiException(400, "Account Already Exist");
+            throw new ApiException(400, "Error Registering Account");
         }
     }
 
     @PostMapping(value = "/login")
-    public Account login(@RequestBody LoginRequest info) {
-        System.out.println(info.toString());
-
-        return null;
+    public ResponseEntity<?> login(@RequestBody LoginRequest info) throws Exception {
+//        authenticate(info.getEmail(), info.getPassword());
+        final UserDetails userDetails = accountService.loadUserInfo(info.getEmail());
+        final Account user = accountService.getAccount(info.getEmail());
+        final String token = jwtTokenUtil.generateToken(userDetails);
+        LoginResponse resp = new LoginResponse(user, EncryptionUtil.encrypt(token));
+        return ResponseEntity.ok(resp);
     }
 
 }
